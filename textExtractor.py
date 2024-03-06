a3844f45bcf35050c2e1036be4ca88725f584054
Aaron Enyetu <aaronenyetu@gmail.com>
2024-03-06 10:44:15 -0800

    created the form label and text area

--- textExtractor.py
+++ textExtractor.py
@@ -1,2 +1,31 @@
 import tkinter, PyPDF2
 from tkinter import filedialog 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root = tkinter.Tk()
+root.title("PDF Text Extractor")
+
+
+filename_label = tkinter.Label(root, text="No File Selected")
+outputfile_text = tkinter.Text(root)
+
+
+filename_label.pack()
+outputfile_text.pack()
+
+
+root.mainloop()
